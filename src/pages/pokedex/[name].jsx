88e628c92c46969eec83958b9cc53d6bfcaed559
FedericaI7 <federicaiuvara3@gmail.com>
2024-02-02 21:36:42 +0100

    Add: input on index.jsx(Pokedex) and call API on [name].jsx(Pokedex)

--- src/pages/pokedex/[name].jsx
+++ src/pages/pokedex/[name].jsx
@@ -1,5 +1,20 @@
 import styles from "../../styles/Pokedex.module.scss";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Pokedex() {
-  return <div className={styles.Pokedex}></div>;
+  const [pokemon, setPokemon] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${router.query.name}`)
+      .then((res) => res.json())
+      .then((data) => setPokemon(data));
+  }, [router.query.name]);
+
+  return (
+    <main className={styles.Pokedex}>
+      <h1>{pokemon.name?.charAt(0).toUpperCase() + pokemon.name?.slice(1)}</h1>
+    </main>
+  );
 }
--- src/pages/pokedex/index.jsx
+++ src/pages/pokedex/index.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
 import styles from "../../styles/Pokedex.module.scss";
+import { useRouter } from "next/router";
 
 export default function Pokedex() {
-  return <div className={styles.Pokedex}>hello</div>;
+  const [valueInput, setValueInput] = useState("");
+  const router = useRouter();
+
+  const onHandleInput = (e) => {
+    setValueInput(e.target.value.toLowerCase());
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValueInput("");
+    router.push(`/pokedex/${valueInput}`);
+  };
+
+  return (
+    <div className={styles.Pokedex}>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={valueInput}
+          onChange={onHandleInput}
+          type="text"
+          placeholder="Search your pokemon"
+        />
+      </form>
+    </div>
+  );
 }
