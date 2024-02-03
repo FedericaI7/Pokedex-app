4b1da16167d2cfd5e689fe48ad4c40dc23210739
FedericaI7 <federicaiuvara3@gmail.com>
2024-02-03 19:53:26 +0100

    Add/style: add Image, type, and style

--- src/pages/pokedex/[name].jsx
+++ src/pages/pokedex/[name].jsx
@@ -1,6 +1,8 @@
 import styles from "../../styles/Pokedex.module.scss";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Pokedex() {
   const [pokemon, setPokemon] = useState({});
@@ -27,6 +29,29 @@ export default function Pokedex() {
           <h1>
             {pokemon.name?.charAt(0).toUpperCase() + pokemon.name?.slice(1)}
           </h1>
+
+          {/* type */}
+          <div className={styles.containerTypes}>
+            {pokemon.types &&
+              pokemon.types.map((el) => (
+                <p className={styles.type} key={el.type.name}>
+                  {el.type.name}
+                </p>
+              ))}
+          </div>
+          <div className={styles.containerImgandNumber}>
+            <Image
+              className={styles.imgPokemon}
+              src={
+                pokemon.sprites &&
+                pokemon.sprites.other?.["official-artwork"]?.front_default
+              }
+              width={1000}
+              height={1000}
+              alt={pokemon.name + " picture"}
+            />
+            <span>{"#" + pokemon.order}</span>
+          </div>
         </div>
       )}
     </div>
