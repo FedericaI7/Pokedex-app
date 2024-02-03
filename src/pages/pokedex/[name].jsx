c010a20dd270feebcb360e109c4f12ba495469f3
FedericaI7 <federicaiuvara3@gmail.com>
2024-02-03 15:10:09 +0100

    Feat: Add new API call to retrieve Pokemon species data

--- src/pages/pokedex/[name].jsx
+++ src/pages/pokedex/[name].jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 
 export default function Pokedex() {
   const [pokemon, setPokemon] = useState({});
+  const [pokemonSpecies, setPokemonSpecies] = useState({});
   const router = useRouter();
 
   useEffect(() => {
@@ -11,10 +12,23 @@ export default function Pokedex() {
       .then((res) => res.json())
       .then((data) => setPokemon(data));
   }, [router.query.name]);
+  useEffect(() => {
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon-species/${router.query.name}/?language_id=9`
+    )
+      .then((res) => res.json())
+      .then((data) => setPokemonSpecies(data));
+  }, [router.query.name]);
 
   return (
-    <main className={styles.Pokedex}>
-      <h1>{pokemon.name?.charAt(0).toUpperCase() + pokemon.name?.slice(1)}</h1>
-    </main>
+    <div className={styles.containerPokedex}>
+      {pokemon && pokemonSpecies && (
+        <div className={styles.Pokedex}>
+          <h1>
+            {pokemon.name?.charAt(0).toUpperCase() + pokemon.name?.slice(1)}
+          </h1>
+        </div>
+      )}
+    </div>
   );
 }
