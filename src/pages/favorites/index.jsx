4a48cf6db78b768862072484ff9edff4038b7335
FedericaI7 <federicaiuvara3@gmail.com>
2024-02-09 15:08:16 +0100

    Style: change responsive style in SearchPokedex.module.scss

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -151,7 +151,6 @@ export default function Favorites() {
             ))}
           </section>
         </div>
-        ;
       </main>
     </>
   );
--- src/pages/pokedex/index.jsx
+++ src/pages/pokedex/index.jsx
@@ -95,52 +95,50 @@ export default function Pokedex() {
             ></Image>
           </div>
         </div>
-        <div className={styles.containerBottom}>
-          <section className={styles.sectionCard}>
-            {/* Card Section Pokemon  */}
-            {pokemonData.map((pokemon, index) => (
-              <div
-                key={index}
-                className={styles.cardPokemon}
-                style={{
-                  backgroundColor: colorBackPokemon(pokemon.species),
-                }}
-              >
-                <div>
-                  {/* ----Number---- */}
-                  {
-                    <span className={styles.numberPokemon}>
-                      {"#" + pokemon.order}
-                    </span>
-                  }
-                  <h2>
-                    {/* ---Name---- */}
-                    {pokemon.name?.charAt(0).toUpperCase() +
-                      pokemon.name?.slice(1)}
-                  </h2>
-                  <div className={styles.containerTypes}>
-                    {/* ---Types---- */}
-                    {pokemon.types &&
-                      pokemon.types.map((el) => (
-                        <p className={styles.type} key={el.type.name}>
-                          {el.type.name}
-                        </p>
-                      ))}
-                  </div>
+
+        <section className={styles.sectionCard}>
+          {/* Card Section Pokemon  */}
+          {pokemonData.map((pokemon, index) => (
+            <div
+              key={index}
+              className={styles.cardPokemon}
+              style={{
+                backgroundColor: colorBackPokemon(pokemon.species),
+              }}
+            >
+              <div>
+                {/* ----Number---- */}
+                {
+                  <span className={styles.numberPokemon}>
+                    {"#" + pokemon.order}
+                  </span>
+                }
+                <h2>
+                  {/* ---Name---- */}
+                  {pokemon.name?.charAt(0).toUpperCase() +
+                    pokemon.name?.slice(1)}
+                </h2>
+                <div className={styles.containerTypes}>
+                  {/* ---Types---- */}
+                  {pokemon.types &&
+                    pokemon.types.map((el) => (
+                      <p className={styles.type} key={el.type.name}>
+                        {el.type.name}
+                      </p>
+                    ))}
                 </div>
-                {/* ----Image---- */}
-                <Image
-                  className={styles.imgPokemon}
-                  width={100}
-                  height={50}
-                  src={pokemon.sprites.other.dream_world.front_default}
-                  alt={`image pokemon ${pokemon.name}`}
-                />
               </div>
-            ))}
-          </section>
-        </div>
-        ;
+              {/* ----Image---- */}
+              <Image
+                className={styles.imgPokemon}
+                width={100}
+                height={50}
+                src={pokemon.sprites.other.dream_world.front_default}
+                alt={`image pokemon ${pokemon.name}`}
+              />
+            </div>
+          ))}
+        </section>
       </main>
     </>
   );
